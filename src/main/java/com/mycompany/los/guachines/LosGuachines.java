1ba65ee2c2b72849a733b0bd891f8a0a0a02fd0b
j1-dev <aephyxen@gmail.com>
2024-02-14 13:09:52 +0100

    Proyecto inicializado

---
+++ src/main/java/com/mycompany/los/guachines/LosGuachines.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.los.guachines;
+
+/**
+ *
+ * @author j1
+ */
+public class LosGuachines {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
