61329841a34154436d56b3ab54a9a5abf8cd947f
j1-dev <aephyxen@gmail.com>
2024-02-14 14:21:15 +0100

    Codificacion de las clases

---
+++ src/main/java/com/losguachines/dto/Comanda.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.losguachines.dto;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author j1
+ */
+public class Comanda {
+    private ArrayList<Producto> listaProductos;
+    private Mesa mesa;
+    private LocalDateTime fecha;
+    private String observaciones;
+    
+    public Comanda () {}
+
+    public Comanda(ArrayList<Producto> listaProductas, Mesa mesa, LocalDateTime fecha, String observaciones) {
+        this.listaProductos = listaProductas;
+        this.mesa = mesa;
+        this.fecha = fecha;
+        this.observaciones = observaciones;
+    }
+
+    public ArrayList<Producto> getListaProductos() {
+        return listaProductos;
+    }
+
+    public void setListaProductos(ArrayList<Producto> listaProductas) {
+        this.listaProductos = listaProductas;
+    }
+
+    public Mesa getMesa() {
+        return mesa;
+    }
+
+    public void setMesa(Mesa mesa) {
+        this.mesa = mesa;
+    }
+
+    public LocalDateTime getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDateTime fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+    
+    
+    
+    public float getTotal() {
+        float total = 0f;
+        
+        for (int i = 0; i < listaProductos.size(); i++) {
+            total += listaProductos.get(i).getPrecio();
+        }
+        
+        return total;
+    }
+}
